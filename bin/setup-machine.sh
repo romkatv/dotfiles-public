890942d4ed00c12963cd128ddd9a044976bf304f
romkatv <roman.perepelitsa@gmail.com>
2019-03-01 19:49:22 +0100

    install gitstatus plugin

--- bin/setup-machine.sh
+++ bin/setup-machine.sh
@@ -186,6 +186,8 @@ install_fonts
 install_ohmyzsh
 
 install_ohmyzsh_extension plugin \
+  gitstatus git@github.com:romkatv/gitstatus.git
+install_ohmyzsh_extension plugin \
   zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting.git
 install_ohmyzsh_extension plugin \
   zsh-autosuggestions -b faster-counts git@github.com:romkatv/zsh-autosuggestions.git
