a8cd066204afa37abbd9c7919d099b5bba452f0b
romkatv <roman.perepelitsa@gmail.com>
2019-08-19 17:22:06 +0200

    better completions

--- .zshrc
+++ .zshrc
@@ -79,12 +79,18 @@ ZLE_REMOVE_SUFFIX_CHARS=''     # don't eat space when typing '|' after a tab com
 
 setopt ALWAYS_TO_END           # full completions move cursor to the end
 setopt AUTO_CD                 # `dirname` is equivalent to `cd dirname`
+setopt AUTO_LIST               # automatically list choices on ambiguous completion
+setopt AUTO_MENU               # show completion menu on a successive tab press
+setopt AUTO_PARAM_SLASH        # if completed parameter is a directory, add a trailing slash
 setopt AUTO_PUSHD              # `cd` pushes directories to the directory stack
+setopt COMPLETE_IN_WORD        # complete from both ends of a word
+setopt EXTENDED_GLOB           # needed for file modification glob modifiers with compinit
 setopt EXTENDED_GLOB           # (#qx) glob qualifier and more
+setopt EXTENDED_HISTORY        # write timestamps to history
 setopt GLOB_DOTS               # glob matches files starting with dot; `*` becomes `*(D)`
 setopt HIST_EXPIRE_DUPS_FIRST  # if history needs to be trimmed, evict dups first
-setopt HIST_IGNORE_DUPS        # don't add dups to history
 setopt HIST_FIND_NO_DUPS       # don't show dups when searching history
+setopt HIST_IGNORE_DUPS        # don't add dups to history
 setopt HIST_IGNORE_SPACE       # don't add commands starting with space to history
 setopt HIST_REDUCE_BLANKS      # remove junk whitespace from commands before adding to history
 setopt HIST_VERIFY             # if a cmd triggers history expansion, show it instead of running
@@ -92,11 +98,10 @@ setopt INTERACTIVE_COMMENTS    # allow comments in command line
 setopt MULTIOS                 # allow multiple redirections for the same fd
 setopt NO_BANG_HIST            # disable old history syntax
 setopt NO_BG_NICE              # don't nice background jobs; not useful and doesn't work on WSL
+setopt NO_FLOW_CONTROL         # disable start/stop characters in shell editor
+setopt NO_MENU_COMPLETE        # do not autoselect the first completion entry
+setopt PATH_DIRS               # perform path search even on command names with slashes
 setopt SHARE_HISTORY           # write and import history on every command
-setopt EXTENDED_HISTORY        # write timestamps to history
-
-# setopt COMPLETE_IN_WORD      # not sure what it does
-# setopt NO_FLOW_CONTROL       # not sure what it does
 
 # export PYENV_ROOT="$HOME/.pyenv"
 # path=("$PYENV_ROOT/bin" $path)
--- dotfiles/completions.zsh
+++ dotfiles/completions.zsh
@@ -1,20 +1,13 @@
-zstyle ':completion:*'                    list-colors    "${(s.:.)LS_COLORS}"
-zstyle ':completion:*:*:*:*:*'            menu           'select'
-zstyle ':completion:*'                    matcher-list   'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*'                    special-dirs   'true'
-zstyle ':completion:*:*:kill:*:processes' list-colors    '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:*:*:processes'    command        'ps -u $USER -o pid,user,comm -w -w'
-zstyle ':completion::complete:*'          use-cache      '1'
-zstyle '*'                                single-ignored 'show'
-zstyle ':completion:*:sudo:*'             command-path   '/usr/local/sbin' \
-                                                         '/usr/local/bin'  \
-                                                         '/usr/sbin'       \
-                                                         '/usr/bin'        \
-                                                         '/sbin'           \
-                                                         '/bin'            \
-                                                         '/usr/X11R6/bin'
+# Based on:
+# https://github.com/sorin-ionescu/prezto/blob/6f603df7a641fb136b8b168d75e905fef60a00cf/modules/completion/init.zsh.
+#
+# Which is released under MIT license:
+# https://github.com/sorin-ionescu/prezto/blob/6f603df7a641fb136b8b168d75e905fef60a00cf/LICENSE.
 
-autoload -U compinit
+# Load and initialize the completion system ignoring insecure directories with a
+# cache time of 20 hours, so it should almost always regenerate the first time a
+# shell is opened each day.
+autoload -Uz compinit
 compinit
 
 () {
@@ -23,3 +16,114 @@ compinit
     zcompile $zcompdump
   fi
 }
+ 
+# Use caching to make completion for commands such as dpkg and apt usable.
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache"
+
+# Case-insensitive (all), partial-word, and then substring completion.
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Group matches and describe.
+zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*' list-dirs-first true
+
+# Fuzzy match mistyped completions.
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+# Increase the number of errors based on the length of the typed word. But make
+# sure to cap (at 7) the max-errors to avoid hanging.
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3>7?7:($#PREFIX+$#SUFFIX)/3))numeric)'
+
+# Don't complete unavailable commands.
+zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
+
+# Array completion element sorting.
+zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
+
+# Directories.
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
+zstyle ':completion:*' squeeze-slashes true
+
+# History.
+zstyle ':completion:*:history-words' stop yes
+zstyle ':completion:*:history-words' remove-all-dups yes
+zstyle ':completion:*:history-words' list false
+zstyle ':completion:*:history-words' menu yes
+
+# Environment Variables.
+zstyle ':completion::*:(-command-|export):*' fake-parameters ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
+
+# Populate hostname completion. But allow ignoring custom entries from static
+# */etc/hosts* which might be uninteresting.
+zstyle -a ':prezto:module:completion:*:hosts' etc-host-ignores '_etc_host_ignores'
+
+zstyle -e ':completion:*:hosts' hosts 'reply=(
+  ${=${=${=${${(f)"$(cat {/etc/ssh/ssh_,~/.ssh/}known_hosts(|2)(N) 2> /dev/null)"}%%[#| ]*}//\]:[0-9]*/ }//,/ }//\[/ }
+  ${=${(f)"$(cat /etc/hosts(|)(N) <<(ypcat hosts 2> /dev/null))"}%%(\#${_etc_host_ignores:+|${(j:|:)~_etc_host_ignores}})*}
+  ${=${${${${(@M)${(f)"$(cat ~/.ssh/config 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
+)'
+
+# Don't complete uninteresting users...
+zstyle ':completion:*:*:*:users' ignored-patterns \
+  adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
+  dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
+  hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
+  mailman mailnull mldonkey mysql nagios \
+  named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
+  operator pcap postfix postgres privoxy pulse pvm quagga radvd \
+  rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs '_*'
+
+# ... unless we really want to.
+zstyle '*' single-ignored show
+
+# Ignore multiple entries.
+zstyle ':completion:*:(rm|kill|diff):*' ignore-line other
+zstyle ':completion:*:rm:*' file-patterns '*:all-files'
+
+# Kill.
+zstyle ':completion:*:*:*:*:processes' command 'ps -u $LOGNAME -o pid,user,command -w'
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
+zstyle ':completion:*:*:kill:*' menu yes select
+zstyle ':completion:*:*:kill:*' force-list always
+zstyle ':completion:*:*:kill:*' insert-ids single
+
+# Man.
+zstyle ':completion:*:manuals' separate-sections true
+zstyle ':completion:*:manuals.(^1*)' insert-sections true
+
+# Media Players.
+zstyle ':completion:*:*:mpg123:*' file-patterns '*.(mp3|MP3):mp3\ files *(-/):directories'
+zstyle ':completion:*:*:mpg321:*' file-patterns '*.(mp3|MP3):mp3\ files *(-/):directories'
+zstyle ':completion:*:*:ogg123:*' file-patterns '*.(ogg|OGG|flac):ogg\ files *(-/):directories'
+zstyle ':completion:*:*:mocp:*' file-patterns '*.(wav|WAV|mp3|MP3|ogg|OGG|flac):ogg\ files *(-/):directories'
+
+# Mutt.
+if [[ -s "$HOME/.mutt/aliases" ]]; then
+  zstyle ':completion:*:*:mutt:*' menu yes select
+  zstyle ':completion:*:mutt:*' users ${${${(f)"$(<"$HOME/.mutt/aliases")"}#alias[[:space:]]}%%[[:space:]]*}
+fi
+
+# ssh/scp/rsync.
+zstyle ':completion:*:(ssh|scp|rsync):*' tag-order 'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
+zstyle ':completion:*:(scp|rsync):*' group-order users files all-files hosts-domain hosts-host hosts-ipaddr
+zstyle ':completion:*:ssh:*' group-order users hosts-domain hosts-host users hosts-ipaddr
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns '*(.|:)*' loopback ip6-loopback localhost ip6-localhost broadcasthost
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain' ignored-patterns '<->.<->.<->.<->' '^[-[:alnum:]]##(.[-[:alnum:]]##)##' '*@*'
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr' ignored-patterns '^(<->.<->.<->.<->|(|::)([[:xdigit:].]##:(#c,2))##(|%*))' '127.0.0.<->' '255.255.255.255' '::1' 'fe80::*'
