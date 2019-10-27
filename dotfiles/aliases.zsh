5ea9ea68f3477cec9d28a622ef960d55a6ad14b8
romkatv <roman.perepelitsa@gmail.com>
2019-10-27 14:09:51 +0100

    use xclip instead of xsel on linux (xsel hangs Atom); not sure if xclip also works on wsl

--- dotfiles/aliases.zsh
+++ dotfiles/aliases.zsh
@@ -1,24 +1,21 @@
 alias diff='diff --color=auto'
 alias grep='grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
 alias clang-format='clang-format -style=file'
-alias ls='ls --group-directories-first --color=auto'
+alias ls='ls --color=auto --group-directories-first'
 alias tree='tree -aC -I .git --dirsfirst'
 alias gedit='gedit &>/dev/null'
-alias d2u='dos2unix'
-alias u2d='unix2dos'
 
-if [[ -d ~/.dotfiles-public ]]; then
-  alias dotfiles-public='git --git-dir="$HOME"/.dotfiles-public/.git --work-tree="$HOME"'
-fi
-if [[ -d ~/.dotfiles-private ]]; then
-  alias dotfiles-private='git --git-dir="$HOME"/.dotfiles-private/.git --work-tree="$HOME"'
-fi
-
-alias x='xsel --clipboard -i'  # cut to clipboard
-alias v='xsel --clipboard -o'  # paste from clipboard
-alias c='x && v'               # copy to clipboard
+alias dotfiles-public='git --git-dir="$HOME"/.dotfiles-public/.git --work-tree="$HOME"'
+alias dotfiles-private='git --git-dir="$HOME"/.dotfiles-private/.git --work-tree="$HOME"'
 
 if (( WSL )); then
   hash -d r=/mnt/d/r
   hash -d h="$(wslpath "$(win_env USERPROFILE)")"
+  alias x='xsel --clipboard -i'                     # cut to clipboard
+  alias v='xsel --clipboard -o'                     # paste from clipboard
+  alias c='{ x && v }'                              # copy to clipboard
+else
+  alias x='xclip -selection clipboard -in'          # cut to clipboard
+  alias v='xclip -selection clipboard -out'         # paste from clipboard
+  alias c='xclip -selection clipboard -in -filter'  # copy clipboard
 fi
