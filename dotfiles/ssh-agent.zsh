002947330ff40050f01063d4624dc90a55174822
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-06-04 19:28:53 +0200

    ssh-agent.zsh cleanup

--- dotfiles/ssh-agent.zsh
+++ dotfiles/ssh-agent.zsh
@@ -1,34 +1,39 @@
 # Starts ssh-agent if it isn't already running.
 () {
-  emulate -L zsh
-  setopt err_return no_unset extended_glob
+  emulate -L zsh -o no_unset -o extended_glob
 
   function _ssh-agent-running() {
-    unsetopt err_return
     [[ -v SSH_AUTH_SOCK && -r $SSH_AUTH_SOCK && -w $SSH_AUTH_SOCK &&
        -v SSH_AGENT_PID && "$(ps -p $SSH_AGENT_PID -o comm= 2>/dev/null)" == ssh-agent ]]
   }
-  trap 'unfunction _ssh-agent-running' EXIT
 
-  ! _ssh-agent-running || return 0
+  {
+    _ssh-agent-running && return
 
-  local env_file=${XDG_CACHE_HOME:-$HOME/.cache}/ssh-agent-env
-  mkdir -pm 0700 ${env_file:h}
+    local env_file=${XDG_CACHE_HOME:-$HOME/.cache}/ssh-agent-env
+    if [[ -d ${env_file:h} ]]; then
+      command mkdir -pm 0700 -- ${env_file:h} || return
+    fi
 
-  local f=$env_file
-  while true; do
-    [[ -z $f(#qNW) ]] || {
-      echo "Not using ssh-agent because ${(qqq)f} is world-writable." >&2
-      return 1 
-    }
-    [[ $f != / ]] || break
-    f=${f:h}
-  done
+    local f=$env_file
+    while true; do
+      if [[ -n $f(#qNW) ]]; then
+        echo "Not using ssh-agent because ${(qqq)f} is world-writable." >&2
+        return 1 
+      fi
+      [[ $f == / ]] && break
+      f=${f:h}
+    done
 
-  [[ ! -e $env_file ]] || source $env_file >/dev/null
-  ! _ssh-agent-running || return 0
+    if [[ -e $env_file ]]; then
+      source $env_file >/dev/null
+      _ssh-agent-running && return
+    fi
 
-  unset SSH_AGENT_PID SSH_AUTH_SOCK
-  ssh-agent -st 20h >$env_file
-  source $env_file >/dev/null
+    unset SSH_AGENT_PID SSH_AUTH_SOCK
+    command ssh-agent -st 20h >$env_file || return
+    source $env_file >/dev/null          || return
+  } always {
+    unfunction _ssh-agent-running
+  }
 }
