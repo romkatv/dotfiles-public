f559fb7bc5883740820a862427f6fb79630dd7a0
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-07-28 10:24:10 +0200

    clean up ssh-agent.zsh

--- dotfiles/ssh-agent.zsh
+++ dotfiles/ssh-agent.zsh
@@ -4,35 +4,44 @@
 
   function _ssh-agent-running() {
     [[ -v SSH_AUTH_SOCK && -r $SSH_AUTH_SOCK && -w $SSH_AUTH_SOCK &&
-       -v SSH_AGENT_PID && "$(ps -p $SSH_AGENT_PID -o comm= 2>/dev/null)" == ssh-agent ]]
+       -v SSH_AGENT_PID && "$(command ps -p $SSH_AGENT_PID -o comm= 2>/dev/null)" == ssh-agent ]]
   }
 
   {
     _ssh-agent-running && return
 
     local env_file=${XDG_CACHE_HOME:-$HOME/.cache}/ssh-agent-env
-    if [[ -d ${env_file:h} ]]; then
+    if [[ ! -d ${env_file:h} ]]; then
       command mkdir -pm 0700 -- ${env_file:h} || return
     fi
 
-    local f=$env_file
-    while true; do
-      if [[ -n $f(#qNW) ]]; then
-        echo "Not using ssh-agent because ${(qqq)f} is world-writable." >&2
-        return 1 
-      fi
-      [[ $f == / ]] && break
-      f=${f:h}
-    done
+    if [[ -n $env_file(#qNW) || -n ${env_file:h}/(../)#(#qNW) ]]; then
+      local f=$env_file
+      while true; do
+        if [[ -n $f(#qNW) ]]; then
+          f=${(q)f//\%/%%}
+          print -ru2 -- ${(%):-"Not using ssh-agent because %U$f%u is %F{1}world-writable%f."}
+          print -ru2 -- ""
+          print -ru2 -- "Run the following command to fix this issue:"
+          print -ru2 -- ""
+          print -ru2 -- ${(%):-"  %F{2}%Usudo%u chmod%f o-w -- %U$f%u"}
+          return 1 
+        fi
+        [[ $f == / ]] && break
+        f=${f:h}
+      done
+    fi
+
+    return
 
     if [[ -e $env_file ]]; then
-      source $env_file >/dev/null
+      builtin source $env_file >/dev/null
       _ssh-agent-running && return
     fi
 
     unset SSH_AGENT_PID SSH_AUTH_SOCK
     command ssh-agent -st 20h >$env_file || return
-    source $env_file >/dev/null          || return
+    builtin source $env_file >/dev/null  || return
   } always {
     unfunction _ssh-agent-running
   }
